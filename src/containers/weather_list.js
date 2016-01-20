4f79f2313ce4f0c1f73c2cc2fc7694acd66b648e
Nick Arora <arora.nick@gmail.com>
2016-01-19 20:39:43 -0800

    add average and units

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -13,9 +13,9 @@ class WeatherList extends Component {
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td><Chart data={temps} color="orange" /></td>
-                <td><Chart data={pressures} color="blue" /></td>
-                <td><Chart data={humidities} color="red" /></td>
+                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={pressures} color="blue" units="hPa" /></td>
+                <td><Chart data={humidities} color="red" units="%" /></td>
             </tr>
         );
 
@@ -29,9 +29,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>

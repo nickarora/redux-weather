de870a4bcf9ed153e3a9034d0519f8d66c4fbfda
Nick Arora <arora.nick@gmail.com>
2016-01-19 20:33:10 -0800

    generate charts for pressures and humidities

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,25 +6,16 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => {
-            return weather.main.temp;
-
-        });
-
-        const humidities = cityData.list.map(weather => {
-            return weather.main.humidity
-        });
-
-        const pressures = cityData.list.map(weather => {
-            return weather.main.pressure
-        });
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td>
-                    <Chart data={temps} color="red" />
-                </td>
+                <td><Chart data={temps} color="orange" /></td>
+                <td><Chart data={pressures} color="blue" /></td>
+                <td><Chart data={humidities} color="red" /></td>
             </tr>
         );
 
